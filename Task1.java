a85052c32ee08975068b67c86e28965096870144
Денис Пономарев <deskin196@gmail.com>
2023-06-25 12:45:39 +0300

    Написал функцию по задаче 1

--- Task1.java
+++ Task1.java
@@ -7,6 +7,25 @@
 public class Task1
 {
     public static void main(String[] args) {
-        System.out.println("cvfdsfds");
+        String json = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}";
+        System.out.println(sql(json));
+    }
+    public static String sql(String json) {
+        //Удаляем фигурные скобки
+        json = json.replaceAll("\\{|\\}", "");
+        StringBuilder sql = new StringBuilder("select * from students where ");
+        //Разделяем нашу строку с json на массив по запятой
+        String[] array = json.split(",");
+
+        for (String string : array) {
+            //Достаем ключ удаляем пробелы по краям и удаляем кавычки
+            String key = string.split(":")[0].trim().replace("\"", "");
+            //Достаем значение удаляем пробелы по краям и меняем одинарные кавычки на двойные
+            String value = string.split(":")[1].trim().replace("\"", "'");
+            if(!value.equals("\'null\'")) sql.append(key).append("=").append(value).append(" AND ");
+        }
+        //Удаляем последнюю end и возвращаем результат
+        return sql.delete(sql.length() - 5, sql.length()-1).toString();
+        
     }
 }
