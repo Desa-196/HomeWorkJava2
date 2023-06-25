77f3c3beece009cf53360607ee9295fe7ec081b8
Денис Пономарев <deskin196@gmail.com>
2023-06-25 13:24:11 +0300

    Написал функцию добавления лога в конец файла по задаче 2

--- Task2.java
+++ Task2.java
@@ -1,6 +1,24 @@
+/* 2)* Реализуйте алгоритм сортировки пузырьком числового массива, 
+результат после каждой итерации запишите в лог-файл. */
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Task2
 {
+    public static String file = "log.txt";
     public static void main(String[] args) {
-
+        int[] array = new int[]{0,4,7,2,34,1,6,32,6};
     }
+    public static void writeLog(String text){
+        try(FileWriter writer = new FileWriter(file, true)){ 
+            writer.append(text); 	      
+        } 	
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
 }
