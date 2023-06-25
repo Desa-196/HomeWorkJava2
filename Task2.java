01c1ee6deaba653114a9325ffd4d408bc0e6d383
Денис Пономарев <deskin196@gmail.com>
2023-06-25 13:24:36 +0300

    Написал функцию сортировки по задаче 2

--- Task2.java
+++ Task2.java
@@ -11,6 +11,7 @@ public class Task2
     public static String file = "log.txt";
     public static void main(String[] args) {
         int[] array = new int[]{0,4,7,2,34,1,6,32,6};
+        bubbleSort(array);
     }
     public static void writeLog(String text){
         try(FileWriter writer = new FileWriter(file, true)){ 
@@ -20,5 +21,16 @@ public class Task2
             System.out.println(e);
         }
     }
-
+    public static void bubbleSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for(int j = 0; j < array.length - i - 1; j++) {
+                if(array[j + 1] < array[j]) {
+                    int swap = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = swap;
+                }
+                writeLog(Arrays.toString(array)+'\n');
+            }
+        }
+    }
 }
